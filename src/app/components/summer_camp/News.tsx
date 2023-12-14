4643e04302ed7c2be67041b9bf9bf1f1eaac3e35
strawberryisamu <kiuchi-isamu@life-coach.co.jp>
2023-12-14 18:29:00 +0900

    ニュースデザイン修正

--- src/app/components/summer_camp/News.tsx
+++ src/app/components/summer_camp/News.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 const News: React.FC = () => {
   return (
-    <div className="flex flex-col text-black ">
-      <div className="w-full pl-[5%] m-auto bg-black pl-[1rem] pt-10 pb-10">
+      <div className='pl-[5%] bg-white flex pt-10 pb-10'>
         <h2 className="font-bold text-5xl text-white text-yellow-500">News</h2>
-      </div>
-      <div className='pl-[10%] pt-5 pb-5 '>
-        <div className='flex flex-col md:flex-row items-center pb-1'>
-          <p className="mr-[1rem] font-light">2023-12-03</p>
-          <p className="text-xl">The catching up party was held in Kyoto.</p>
+        <div className='pl-[2.5%]'>
+          <div className='flex flex-col md:flex-row items-center pb-1'>
+            <p className="mr-[1rem] font-light">2023-12-03</p>
+            <p className="text-xl">The catching up party was held in Kyoto.</p>
+          </div>
+          <div className='flex flex-col md:flex-row items-center'>
+            <p className="mr-[1rem] font-light">2023-08-29</p>
+            <p className="text-xl">Rishiri Island Summer Camp 2023 finished successfully.</p>
+          </div>
         </div>
-        <div className='flex flex-col md:flex-row items-center'>
-          <p className="mr-[1rem] font-light">2023-08-29</p>
-          <p className="text-xl">Rishiri Island Summer Camp 2023 finished successfully.</p>
-        </div>
       </div>
-    </div>
   );
 };
 
